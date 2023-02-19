233abcaecdfe5ac168c09f05feae62ce186f8f92
Heather Davy <Davyh4100@clarkstate.edu>
2023-02-18 19:24:54 -0500

    first commit

--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,24 @@ const path = require('path'); // continues the path directly
 
 const express = require('express'); // calling the functions and objects assigned to it
 
-const productsController = require('../controllers/products');
+const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
-router.get('/', productsController.getProducts);
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts); 
+
+router.get('/products/:productId', shopController.getProduct);
+
+router.get('/cart', shopController.getCart); 
+
+router.get('/cart', shopController.postCart);
+
+router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+
+router.get('/orders', shopController.getOrders); 
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router; //this is a variable that represents the current module and exports an object. 
